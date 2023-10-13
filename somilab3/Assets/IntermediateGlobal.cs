6914a14cf2e2400f02ed48646bb963c8e051a787
DiegoRol69 <drol@ucm.es>
2023-10-13 14:04:05 +0200

    cambio de 2 tonterias
    Co-Authored-By: MIRIAM MARTIN <81752837+miriam-m-s@users.noreply.github.com>

--- somilab3/Assets/IntermediateGlobal.cs
+++ somilab3/Assets/IntermediateGlobal.cs
@@ -22,6 +22,7 @@ public class IntermediateGlobal : MonoBehaviour
     public bool pitchVariation = false;
     public float ISound = 0.2f;
     public float ISoundCondition = 0.2f;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -31,71 +32,32 @@ public class IntermediateGlobal : MonoBehaviour
             _Speaker[i] = gameObject.AddComponent<AudioSource>();
             _Speaker[i].playOnAwake = true;
             _Speaker[i].loop = loop_;
-            _Speaker[i].volume = ISound;
         }
 
     }
+
     void Start()
     {
-
-
-    }
-    public AudioSource getChannel()
-    {
-        int ch = 0;
-        while (ch < maxAudioSource && _Speaker[ch].isPlaying) ch++;
-        if (ch < maxAudioSource)
+        if (loop_)
         {
-            return _Speaker[ch];
+            AudioSource source = getChannel();
+            if (enablePlayMode && source)
+            {
+                PlaySound(source);
+            }
         }
-        else return null;
-    }
-    void PlaySound(AudioSource source)
-    {
-        SetSourceProperties(audio[Random.Range(0, audio.Length)], minVol, maxVol, distRand, maxDist, spatialBlend, source);
-        if (pitchVariation)
-            source.pitch = Random.Range(0.95f, 1.05f);
-        Debug.Log($"Canal  Sample  pitch {_Speaker[0].pitch}");
-        if (ISound >= ISoundCondition)
-            source.Play();
-
 
     }
-
-
-
-    public void SetSourceProperties(AudioClip audioData, float minVol, float maxVol,
-                                    int minDist, int maxDist, float SpatialBlend, AudioSource source)
-    {
-        source.loop = false;
-        source.maxDistance = maxDist - Random.Range(0f, distRand);
-        source.spatialBlend = spatialBlend;
-        source.clip = audioData;
-        source.volume = SourceVol + Random.Range(minVol, maxVol);
-    }
-
-
-
-
-    void StopSound()
-    {
-        enablePlayMode = false;
-        Debug.Log("stop");
-    }
-
 
     void Update()
     {
-        
-        StartCoroutine("Waitforit");
+        if (!loop_) StartCoroutine("Waitforit");
     }
+
     IEnumerator Waitforit()
     {
-       
-        float waitTime = Random.Range(minTime, maxTime);
-        if(loop_)waitTime = 0f;
 
-        Debug.Log(waitTime);
+        float waitTime = Random.Range(minTime, maxTime);
         AudioSource source = getChannel();
 
         // miramos si hay un clip asignado al source (sirve para la primera vez q se ejecuta)
@@ -107,16 +69,54 @@ public class IntermediateGlobal : MonoBehaviour
         else
         {
             float lenght_ = 0;
-            if (loop_) lenght_ = 0;
-            else if (source.clip != null) lenght_ = source.clip.length;
+            if (source.clip != null) lenght_ = source.clip.length;
 
             yield return new WaitForSeconds(lenght_ + waitTime);
         }
 
-
         //// si esta activado reproducimos sonido
-        if (enablePlayMode && source) {
-            Debug.Log("sonando "+minTime+ " "+maxTime);
-            PlaySound(source); }
+        if (enablePlayMode && source)
+        {
+            Debug.Log("sonando " + minTime + " " + maxTime);
+            PlaySound(source);
+        }
     }
+
+    public AudioSource getChannel()
+    {
+        int ch = 0;
+        while (ch < maxAudioSource && _Speaker[ch].isPlaying) ch++;
+        if (ch < maxAudioSource)
+        {
+            return _Speaker[ch];
+        }
+        else return null;
+    }
+
+    void PlaySound(AudioSource source)
+    {
+        SetSourceProperties(audio[Random.Range(0, audio.Length)], minVol, maxVol, distRand, maxDist, spatialBlend, source);
+        if (pitchVariation)
+            source.pitch = Random.Range(0.95f, 1.05f);
+        Debug.Log($"Canal  Sample  pitch {_Speaker[0].pitch}");
+        if (ISound >= ISoundCondition)
+            source.Play();
+    }
+
+    public void SetSourceProperties(AudioClip audioData, float minVol, float maxVol,
+                                    int minDist, int maxDist, float SpatialBlend, AudioSource source)
+    {
+        source.maxDistance = maxDist - Random.Range(0f, distRand);
+        source.spatialBlend = spatialBlend;
+        source.clip = audioData;
+        source.volume = SourceVol + Random.Range(minVol, maxVol);
+    }
+
+    void StopSound()
+    {
+        enablePlayMode = false;
+        Debug.Log("stop");
+    }
+
+    
 }
