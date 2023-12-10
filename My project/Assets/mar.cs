01de1e3196aa5b9497c192bcd67c5009299bdd93
MiriamMartín <mirima10@ucm.es>
2023-12-10 16:53:57 +0100

    mar sonorizado

--- My project/Assets/mar.cs
+++ My project/Assets/mar.cs
@@ -7,17 +7,56 @@ using UnityEngine;
 public class mar : MonoBehaviour
 {
     public BGCurve curva;
-    public float distanciaMaxima = 10f;  // Ajusta según sea necesario
-    public float velocidad = 5f;
+    public Transform player;
+
+
     Cursor cursor;
-    private float distanciaRecorrida = 0f;
+    BGCurvePointI[] puntos;
+
+    public GameObject sonidoMar;
     private void Start()
     {
         curva = GetComponent<BGCurve>();
+        puntos=curva.Points;
 
     }
     void Update()
     {
-        
+        // Llama a la función para encontrar el punto más cercano
+        BGCurvePointI puntoCercano = EncontrarPuntoMasCercano(player.position);
+      
+        // Ahora puedes hacer algo con el puntoCercano, como mover el objeto hacia él
+        MoverHaciaPunto(puntoCercano.PositionWorld);
+    }
+    private void MoverHaciaPunto(Vector3 destino)
+    {
+        // Calcula la nueva posición interpolada hacia el destino
+        Vector3 nuevaPosicion = Vector3.Lerp(sonidoMar.transform.position, destino, 5 * Time.deltaTime);
+
+        // Asigna la nueva posición al objeto
+        sonidoMar.transform.position = nuevaPosicion;
+
+        // Puedes agregar aquí cualquier otra lógica o comportamiento adicional que desees
+    }
+    private BGCurvePointI EncontrarPuntoMasCercano(Vector3 posicion)
+    {
+        BGCurvePointI puntoMasCercano = null;
+        float distanciaMinima = float.MaxValue;
+
+        // Itera sobre cada punto de la curva
+        foreach (var punto in puntos)
+        {
+            // Calcula la distancia entre el punto y la posición del jugador
+            float distancia = Vector3.Distance(punto.PositionWorld, posicion);
+
+            // Actualiza el punto más cercano si la distancia actual es menor que la distancia mínima
+            if (distancia < distanciaMinima)
+            {
+                distanciaMinima = distancia;
+                puntoMasCercano = punto;
+            }
+        }
+
+        return puntoMasCercano;
     }
 }
